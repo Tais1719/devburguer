fed0b65a413e4803d27031df0156b8390492c90a
tais1719 <taissantos123098@gmail.com>
2025-08-27 23:03:18 -0300

    Atualizei os estilos

--- src/container/Menu/styles.js
+++ src/container/Menu/styles.js
@@ -1,115 +1,112 @@
 import styled from "styled-components";
-import BannerHamburger from '../../assets/loja4.jpg'
+import BannerHamburger from "../../assets/banner-hamburger.svg"
+import Background from "../../assets/background.svg"
 
 
-import Background from '../../assets/background.jpg'
 import { Link } from "react-router-dom";
 
-
-
 export const Container = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background: ${(props) => props.theme.secondWhite};
 
-width: 100%;
-min-height: 100vh;
-background-color:rgb(95, 231, 10);
- background: linear-gradient(
-      rgba(174, 219, 191, 0.5),
-      rgba(113, 120, 115, 0.5)
-    ),
-    url('${Background}');
-   
-  background-repeat: no-repeat;
-
-
-
-
-
-
-h1{
-   font-family: "Kapakana", cursive;
-    font-size: 60px;
-    line-height: 67px;
-    color:rgb(211, 246, 246);
-    position: absolute;
-   left:5%;
-    top: 30%;
-
-}
-span{
-display: block;
-font-family: "Kapakana", cursive;
-color:#10b981;
-font-size: 50px;
-
-}
+    background:
+            linear-gradient(
+            rgba(255,255,255,0.5),
+            rgba(255,255,255,0.5)
+            ),
+            url('${Background}');
 `
 
-export const Banner = styled.div`
+export const Banner = styled.section`
+    background: url('${BannerHamburger}') no-repeat;
+    background-position: center;
+    background-color: ${(props) => props.theme.mainBlack};
+    background-size: cover;
 
-display: flex;
-justify-content: center;
-height: 400px;
-position: relative;
-background: url('${BannerHamburger}')no-repeat;
-background-color: #1f1f1f;
-background-position: center;
-background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 480px;
+    width: 100%;
+    position: relative;
 
 
 
+    h1{
+        color: ${(props) => props.theme.darkWhite};
+        font-family: "Road Rage", serif;
+        font-size: 80px;
+        line-height: 65px;
+        position: absolute;
+
+        right: 20%;
+        top: 30%;
+    }
+
+    span{
+        display: block;
+        color: ${(props) => props.theme.darkWhite};
+        font-size: 20px;
+        font-weight: 400;
+
+    }
 
 `
 
-
-
-
-
-
-export const CategoryMenu = styled.div`
-display: flex;
-justify-content: center;
-gap: 50px;
-margin-top: 30px;
- 
+export const CategoryMenu = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+    margin-top: 30px;
+    position: relative;
 `
-export const ProductsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr); /* Desktop */
-  padding: 20px;
-  gap: 50px;
-  justify-content: center;
-  max-width: 1200px;
-  margin: 50px auto;
-
-  @media (max-width: 1024px) {
-    grid-template-columns: repeat(3, 1fr); /* Tablets */
-  }
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr); /* Celulares maiores */
-  }
-
-  @media (max-width: 430px) {
-    grid-template-columns: 1fr; /* Celular pequeno: 1 por linha */
-    gap: 30px;
-    padding: 10px;
-  }
-`;
-
-
-
-
 export const CategoryButton = styled(Link)`
-  text-decoration: none;
-  cursor: pointer;
-  background: none;
-  color: ${(props) =>
-    props.$isActiveCategory ? (props).theme. darkBlue : '#36454f'};
-  font-size: 19px;
+    text-decoration: none;
+    background: none;
+    cursor: pointer;
+    color:${props => props.$isActiveCategory ? '#9758A6' : '#696969'}; //se a categoria for ativa, muda a cor
+    font-size: 24px;
+    font-weight: 500;
+    padding-bottom: 5px;
+    line-height: 20px;
+    border: none;
+    border-bottom: ${props => props.$isActiveCategory && '3px solid  #9758A6'}; //se a categoria for ativa, coloca a borda
 
-  padding-bottom: 5px;
-  line-height: 20px;
-  border: none;
-  border-bottom: ${(props) =>
-    props.$isActiveCategory && `3px solid navy`};
-`;
+`
+
+export const ProductsContainer = styled.section`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 40px;
+    gap: 60px;
+    justify-content: center;
+    max-width: 1280px;
+    margin: 50px auto 0;
+`
+
+export const BackButton = styled(Link)`
+    background: #9758A6;
+    border: none;
+    outline: none;
+    color: ${(props) => props.theme.darkWhite};
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    position: absolute;
+    left: 80px;
+    text-decoration: none;
+
+    img{
+        width: 20px;
+        margin-right: 10px;
+        transform: rotate(-180deg);
+    }
+
+    &:hover{
+        background: #7e4c8a;
+    }
+`
